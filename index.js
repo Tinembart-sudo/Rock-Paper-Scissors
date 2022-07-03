927389c8419d513736cd2a0b2947702374d16b48
tinembart-sudo <miguel.tinembart@gmail.com>
2022-07-04 00:41:32 +0200

    Started Array with choices
    - Added const array of choices to pick
    - Changed the name of function intToString

--- index.js
+++ index.js
@@ -1,3 +1,8 @@
+//Initialize Array with 3 Values - Rock, Paper and Cissors in this direction
+//Afterwards check array index of choice
+
+const choiceArray = ["Rock", "Paper", "Scissors"]
+
 /*
 1 = Rock
 2 = Paper
@@ -5,21 +10,22 @@
 */
 
 // Function do determine which number is Rock, Paper, or Scissors
-function intToString(number) {
-    let output
-    switch (number) {
-        case 1:
-            output = "Rock"
-            break;
-        case 2:
-            output = "Paper"
-            break;
-        case 3:
-            output = "Scissors"
-            break;
-        default:
-            output = null
-            break;
+function convertToString(number) {
+    const output = null
+    while (output = null) {
+        switch (number) {
+            case 1:
+                output = "Rock"
+                break;
+            case 2:
+                output = "Paper"
+                break;
+            case 3:
+                output = "Scissors"
+                break;
+            default:
+                break;
+        }
     }
     return output
 }
@@ -30,27 +36,23 @@ function playerOnePick() {
 1 = Rock
 2 = Paper
 3 = Scissors`)
-    return intToString(parseInt(promptinput))
+    return convertToString(parseInt(promptinput))
 }
 
 // player 2 picks with a random number between 1 - 3
 function playerTwoPick() {
     let randomNumber = Math.floor(Math.random() * 3 + 1)
-    return intToString(randomNumber)
+    return convertToString(randomNumber)
 }
 
 
 // return "Win" if user wins, "lose" if users loses and "tie" if its a tie
 function whoIsWinner(choiceOne, choiceTwo) {
-    if (choiceOne == null || choiceTwo == null) {
-        return "Something went wrong here"
+    if (choiceArray.indexOf(choiceOne) == choiceArray.indexOf(choiceTwo)) {
+        return "Tie"
     }
+    
 
-    if (choiceOne == choiceTwo) {
-        return "tie"
-    }
-
-    // Logic for win or lose should be placed here
 
 
 }
